0dceda3c3b48cfcab09dea2810fe9d7725f7f1b1
romanowicz.g <romanowicz.g@pg.com>
2023-10-10 09:22:38 +0200

    aaa

---
+++ main.py
@@ -0,0 +1,10 @@
+class Hello():
+    
+    def __init__(self):
+        self.test="test"
+    
+    def testee(self):
+        print("Hi!")
+        zzz=f"{self.test}, Hello World"
+        return zzz
+    
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test_public',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Lista zależności
+    ],
+)
