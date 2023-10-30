b37e1f5ef6cd253e282f73b20dbb234070ce8c92
romanowicz.g <romanowicz.g@pg.com>
2023-10-30 13:19:39 +0100

    2 teams

---
+++ team_b/hello_module_b.py
@@ -0,0 +1,20 @@
+class HelloB():
+    
+    def __init__(self):
+        self.test="testing"
+    
+    def hello_b(self):
+        print("Hi, team B!")
+        zzz=f"{self.test}, Hello World, team B"
+        return zzz
+
+
+class XxxB():
+    
+    def __init__(self):
+        self.test="testing B"
+    
+    def xxx_b(self):
+        print("xxx, B!")
+        zzz=f"{self.test}, xxx B"
+        return zzz
